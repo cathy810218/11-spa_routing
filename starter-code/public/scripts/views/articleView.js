b78cb865421c0879c968eff3479b7de9b9294746
cathy810218 <cathy810218@gmail.com>
2017-06-27 00:35:24 -0700

    Use page

--- starter-code/public/scripts/views/articleView.js
+++ starter-code/public/scripts/views/articleView.js
@@ -49,18 +49,8 @@ var app = app || {};
     });
   };
 
-  /* TODO: Once the routes are handling '/' and '/about', we can delete
+  /* DONE: Once the routes are handling '/' and '/about', we can delete
       this handleMainNav function. YESSSS! */
-
-  articleView.handleMainNav = function() {
-    $('.main-nav').on('click', '.tab', function() {
-      $('.tab-content').hide();
-      $(`#${$(this).data('content')}`).fadeIn();
-    });
-
-    $('.main-nav .tab:first').click();
-  };
-
   articleView.setTeasers = function() {
     $('.article-body *:nth-of-type(n+2)').hide();
     $('article').on('click', 'a.read-on', function(e) {
@@ -90,11 +80,10 @@ var app = app || {};
         $('#author-filter').append(article.toHtml('#author-filter-template'));
       }
     });
-    /* TODO: Remember to also remove any invocations of handleMainNav... */
+    /* DONE: Remember to also remove any invocations of handleMainNav... */
     articleView.populateFilters();
     articleView.handleCategoryFilter();
     articleView.handleAuthorFilter();
-    articleView.handleMainNav();
     articleView.setTeasers();
     $('pre code').each((i, block) => hljs.highlightBlock(block));
   };
